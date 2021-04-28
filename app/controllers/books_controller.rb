f3ebdbbd105ab8cbbccb977a6b20de8afb8daa41
ark55555 <ark.tooyoung@gmail.com>
2021-04-28 08:48:22 +0000

    [Add]ex-gemfile

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,18 @@
+class BooksController < ApplicationController
+  def index
+  end
+  
+  def create
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+end
